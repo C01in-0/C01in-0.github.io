e326288f841f00f29609d73233128de54f5effc9
C01in-0 <C01in@qq.com>
2026-04-27 00:04:33 +0800

    Site updated: 2026-04-27 00:04:32

--- js/random-subtitle.js
+++ js/random-subtitle.js
@@ -290,7 +290,7 @@
             else if (index === 8) delay += 2000; 
             else if (index === 9) delay += 2000; 
             else if (index === 10) delay += 2800; 
-            else if (index === 11) delay += 4000; 
+            else if (index === 11) delay += 1000; 
         });
 
         setTimeout(function() {
